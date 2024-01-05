c2760bb0b942cae5d876509e4c9ef3f8a2d32006
ahmedazeem <ahmedazeem@rwrlimited.com>
2024-01-05 12:53:03 +0500

    test: added test for example with member functions

--- test/example.cc
+++ test/example.cc
@@ -1,7 +1,36 @@
 #include <sigslot.tpp>
 #include <iostream>
 
+void non_lamb(int x, int y, int z)
+{
+    std::cout<<x<<y<<z<<std::endl;
+}
+
+class data_t
+{
+    int i=0;
+public:
+    data_t(int i): i(i){}
+    void funct(int x,int y,int z)
+    {
+        std::cout<<i<<" -- "<<x<<y<<z<<std::endl;
+    }
+};
+
 int main()
 {
+    data_t dat(9);
+    std::function<void(int, int, int)> 
+        lamb = 
+            [](int x, int y, int z) 
+            { std::cout << x << y << z << std::endl; };
+    Slot_t sig(non_lamb);
+    Signal_t<void,int,int,int> sigi;
+    Signalm_t<data_t,void,int,int,int> sigi2;
+    sigi.connect(sig);
+    Slot_t sig2(lamb);
+    Slotm_t sig3(&data_t::funct);
+    sigi2.connect(dat,sig3);
+    sigi.emit(1,2,3);
     return 0;
 }
